165a432eb912bf4fc25d3d679ae7c3ceadfb2d8b
Seweryn Adamczyk <seweryn.adamczyk88@gmail.com>
2015-09-21 00:07:45 +0200

    Pierwszy commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * Autor kodu: Sewery Adamczyk
+ * Pochodzenie: http://pl.spoj.com/problems/PTEST/
+ */
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner Liczba = new Scanner(System.in);
+		int a = Liczba.nextInt();
+		int b = Liczba.nextInt();
+		System.out.println(a+b);
+		Liczba.close();
+
+	}
+
+}
+
